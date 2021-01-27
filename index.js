f3beb0d5381d9c5e20935ed919a7388376f20317
Randal Schneider <schneider.randal@gmail.com>
2021-01-27 15:33:33 -0600

    reverted back to a previous setup

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Proxy listening at ${port}`)
